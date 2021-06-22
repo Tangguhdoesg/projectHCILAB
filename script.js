6a58dd561905fa63bf3e28a2463a04c517e6dbea
levian14 <levivalentchristian@gmail.com>
2021-06-23 02:17:31 +0700

    image slider revisi

--- script.js
+++ script.js
@@ -17,6 +17,30 @@ function main(){
     let maxIndex = $('.item-slider').length
     let index = 0
     sliding(index)
+
+    //configuration
+    var width = 720;
+    var speed = 1000;
+    var pause = 3000;
+    var current = 1;
+
+    //cache DOM
+    var $slider = $('#slider');
+    var $slides = $slider.find('#slides');
+    var $slide = $slides.find('.slide');
+
+    setInterval(function() {
+        //move image the defined width and speed to the left
+    $slides.animate({'margin-left': '-='+width}, speed, function() {
+        //count number of slides and loop back to first from last
+        current++;
+        if (current === $slides.length) {
+            current = 1;
+            $slides.css('margin-left', 0);
+        }
+    });			
+}, pause);
+    
     $('.prev').click(() => {
         if(index > 0){
             sliding(--index)
